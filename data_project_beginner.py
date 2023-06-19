308d961b6284e24664a573edbe01f42ccb9ba80e
Arjun V P <53475602+arjun1372000@users.noreply.github.com>
2023-06-20 00:59:47 +0530

    created dataframes for the first time

--- data_project_beginner.py
+++ data_project_beginner.py
@@ -8,6 +8,8 @@ import numpy as np
 
 df = pd.read_csv('C:/Users/Arjun V P/Desktop/Pandas/data/survey_results_public.csv')
 print(df.shape)
+
+# Q1
 print("\n\tWhat is the average age at which 1st code was written?")
 
 pd.set_option('display.max_columns', 85)
@@ -24,6 +26,7 @@ df['Age1stCode'] = df["Age1stCode"].replace(['Younger than 5 years', 'Older than
 print('\tAns. {}'.format(df['Age1stCode'].astype(float).mean()))
 
 
+# function to tackle multiple values separate by ';' in a single column value
 def split_column_values(d):
     d = d.dropna().values.tolist()
     lst1 = []
@@ -35,15 +38,19 @@ def split_column_values(d):
     return pd.Series(lst)
 
 
+# function to print the most common value in a series passed
 def most_common(series_passed):
+    # value_counts() returns in descending order hence index[0] will be the most common
     count_series = split_column_values(series_passed).value_counts().index[0]
     print("\t{}  :  {}".format(df.columns[_], count_series))
 
 
+# Q2
 print("\n\tWhat are the most common values in columns 45:54?\n\tAns.")
 for _ in range(45, 54):
     most_common(df.iloc[:, [_]])
 
+# Q3
 print("\n\tConstruct a graph; no:of people who have a SOAccount at that age vs Age.\n\tAns. fig1(var)")
 data = df['Age'].loc[df['SOAccount'] == 'Yes'].value_counts()
 plt.title('No:of people who have an SOAccount vs Age')
@@ -51,6 +58,7 @@ plt.ylabel('No: of people')
 plt.xlabel('Age')
 fig1 = sns.lineplot(data=data)
 
+# Q4
 print("\n\tWhat are the most common values in columns 45:54 w.r.t the OS used?\n\tAns.")
 lst = ['Windows', 'Linux-based', 'MacOS', 'BSD']
 for i in lst:
@@ -58,6 +66,24 @@ for i in lst:
     for _ in range(45, 54):
         most_common(df.iloc[:, [_]].loc[df['OpSys'] == i])
 
-print("\n\tWhat is the most used SocialMedia in each Country?\n\tAns.")
-for country in df['Country'].dropna().unique():
-    print("\t{} : {}".format(country, df['SocialMedia'].loc[df.Country == country].value_counts().index[0]))
+# Q5
+print("\n\tWhat is the most used SocialMedia in each Country?\n\tAns. co_df")
+countries = df['Country'].dropna().unique()
+co_so = []
+for country in countries:
+    temp = df['SocialMedia'].loc[df.Country == country].value_counts().index[0]
+    co_so += [[country, temp]]
+    # print("\t{} : {}".format(country, temp))
+c_df = pd.DataFrame(co_so, columns=['Country', 'SocialMedia'])
+
+# Q6
+print("\n\tWhat is the average, max and min WorkWeekHrs w.r.t Country?\n\tAns. co_hrs_df")
+co_hrs = []
+for country in countries:
+    # nan values means that all the values of WorkWeekHrs that corresponds to that country are nan
+    temp = df['WorkWeekHrs'].loc[df.Country == country].dropna().mean()
+    temp1 = df['WorkWeekHrs'].loc[df.Country == country].dropna().max()
+    temp2 = df['WorkWeekHrs'].loc[df.Country == country].dropna().min()
+    co_hrs += [[country, temp, temp1, temp2]]
+    # print("\t{} : {} {} {}".format(country, temp, temp1, temp2))
+c_hrs_df = pd.DataFrame(co_hrs, columns=['Country', 'MeanWorkWeekHrs', 'MaxWorkWeekHrs', 'MinWorkWeekHrs'])
