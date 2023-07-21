3a4dfc9e2851712d7f38df6755c4f91ffca02364
Arjun1372000 <arjunvp2000@gmail.com>
2023-07-21 12:19:14 +0530

    shorter and possible time optimization(IMPORTANT!)
    Signed-off-by: Arjun1372000 <arjunvp2000@gmail.com>

--- data_project_beginner.py
+++ data_project_beginner.py
@@ -41,7 +41,7 @@ def most_common(series_passed):
 
 
 # Q2
-print("\n\tWhat are the most common values in columns 45:54?\n\tAns.")
+print("\n\tWhat are the most common values in columns 43:54?\n\tAns.")
 for _ in range(43, 54):
     most_common(df.iloc[:, _])
 
@@ -54,42 +54,26 @@ plt.xlabel('Age')
 fig1 = sns.lineplot(data=data)
 
 # Q4
-print("\n\tWhat are the most common values in columns 45:54 w.r.t the OS used?\n\tAns.")
+print("\n\tWhat are the most common values in columns 43:54 w.r.t the OS used?\n\tAns.")
 lst = ['Windows', 'Linux-based', 'MacOS', 'BSD']
 for i in lst:
     print("\n\t{}\n".format(i))
-    for _ in range(45, 54):
+    for _ in range(43, 54):
         most_common(df.iloc[:, _].loc[df['OpSys'] == i])
 
 # Q5
 print("\n\tWhat is the most used SocialMedia in each Country?\n\tAns. co_df")
-countries = df['Country'].dropna().unique()
-co_so = []
-for country in countries:
-    temp = df.loc[df['Country'] == country, 'SocialMedia'].value_counts().index[0]
-    co_so += [[country, temp]]
-    # print("\t{} : {}".format(country, temp))
-c_df = pd.DataFrame(co_so, columns=['Country', 'SocialMedia'])
+# c_df is a Series with Country as index
+c_df = df.groupby('Country')['SocialMedia'].apply(lambda x: x.value_counts().index[0])
 
 # Q6
 print("\n\tWhat is the average, max and min WorkWeekHrs w.r.t Country?\n\tAns. co_hrs_df")
-co_hrs = []
-for country in countries:
-    # nan values means that all the values of WorkWeekHrs that corresponds to that country are nan
-    temp = df.loc[df['Country'] == country, 'WorkWeekHrs'].dropna().mean()
-    temp1 = df.loc[df['Country'] == country, 'WorkWeekHrs'].dropna().max()
-    temp2 = df.loc[df['Country'] == country, 'WorkWeekHrs'].dropna().min()
-    co_hrs += [[country, temp, temp1, temp2]]
-    # print("\t{} : {} {} {}".format(country, temp, temp1, temp2))
-c_hrs_df = pd.DataFrame(co_hrs, columns=['Country', 'MeanWorkWeekHrs', 'MaxWorkWeekHrs', 'MinWorkWeekHrs'])
+# c_hrs_df is a Dataframe with Country as index
+c_hrs_df = df.groupby('Country')['WorkWeekHrs'].agg(['min', 'max', 'mean'])
 
 print("\n\tWhat is the maximum salary(ConvertedComp) w.r.t the Country?\n\tAns. c_comp_df")
-co_comp = []
-for country in countries:
-    temp = df.loc[df['Country'] == country, 'ConvertedComp'].dropna().max()
-    co_comp += [[country, temp]]
-    # print("\t{} : {}".format(country, temp))
-c_comp_df = pd.DataFrame(co_comp, columns=['Country', 'MaxSalaryYearly'])
+# c_comp_df is a Series with Country as index
+c_comp_df = df.groupby('Country')['ConvertedComp'].max()
 
 # Editing of a Column according to a condition
 # df.loc[df['CompFreq'] == 'Monthly', 'ConvertedComp'] *= 12
